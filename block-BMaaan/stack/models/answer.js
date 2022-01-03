00d716ff34421d1e825daf28f9de5eb622bafdbd
Imran Khan <imranlove1997@gmail.com>
2022-01-03 17:58:16 +0530

    Aggregations assignment-2

---
+++ block-BMaaan/stack/models/answer.js
@@ -0,0 +1,20 @@
+let mongoose = require('mongoose');
+
+let Schema = mongoose.Schema;
+
+let answerSchema = new Schema({
+  answer: String,
+  author: { type: Schema.Types.ObjectId, ref: "User" },
+  replies: [{type: Schema.Types.ObjectId, ref: "Reply"}],
+  upVotes: { type: Number, default: 0 },
+  downVotes: { type: Number, default: 0 }
+}, { timestamps: true });
+
+answerSchema.index({upVotes: 1});
+
+db.data.aggregate({$group: {
+  _id: "$answer",
+  count: {sum: 1}
+}});
+
+module.exports = Schema;
